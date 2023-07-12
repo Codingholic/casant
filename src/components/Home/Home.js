88cb76c16ec11c6e5eb45d38c1f9cca598ca2238
Samuel Fernandes <96002830+Codingholic@users.noreply.github.com>
2023-07-12 22:24:45 +0530

    CHECKPOINT 1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,10 @@ import "./Home.css";
 import Navbar from "../Navbar/Navbar";
 import video from "../Navbar/logo/video.mp4";
 import HomePageSecontion2 from "./HomePageSecontion2";
+import whatwedo from "../Navbar/logo/whatwedo.png"
+import lineleft from "../Navbar/logo/double tile.png"
+import lineright from "../Navbar/logo/double tile.png"
+
 const Home = () => {
   const scollToRef = useRef();
   return (
@@ -21,6 +25,12 @@ const Home = () => {
             <source src={video} type="video/webm" />
           </video>
         </div>
+      </div>
+      <div className="dividingText">
+
+        <img className="lineleft" src={lineleft} alt="" />
+        <img className="whatwedo" src={whatwedo} alt="" />
+        <img className="lineright" src={lineright} alt="" />
       </div>
       <HomePageSecontion2 />
     </div>
--- src/components/Home/HomePageSecontion2.js
+++ src/components/Home/HomePageSecontion2.js
@@ -2,7 +2,9 @@ import React from "react"
 import "./HomePageSecontion2.css"
 // import Button from 'react-bootstrap/Button';
 // import Card from 'react-bootstrap/Card';
-import image from "../Navbar/logo/image.png"
+import image2 from "../Navbar/logo/event deco.JPG"
+import image3 from "../Navbar/logo/Sheryl-Crow-Elmo.jpg"
+import image4 from "../Navbar/logo/licensing.jpg"
 // import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
 
@@ -10,11 +12,52 @@ const HomePageSecontion2 = () => {
     return <div className="HomePageSecontion2">
         <div className="card_container_1">
             <div className="service_cards">
-                {/* <img src={image} alt="audio" /> */}
+                <div className="card_image">
+                    <img src={image2} alt="audio" />
+                </div>
+                <h5>Decor & Props</h5>
+                <div className="para_button_container">
+                    <p>Custom designed sets that are tailor-made to suit your requirement...</p>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
 
+                </div>
+                <div className="know_more_Button">
+                    <button className="button-7">know more</button>
+                </div>
             </div>
+            <div className="service_cards">
+                <div className="card_image">
+                    <img src={image3} alt="audio" />
+                </div>
+                <h5>Decor & Props</h5>
+                <div className="para_button_container">
+                    <p>Custom designed sets that are tailor-made to suit your requirement...</p>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
+
+                </div>
+                <div className="know_more_Button">
+                    <button className="button-7">know more</button>
+                </div>
+            </div>
+            <div className="service_cards">
+                <div className="card_image">
+                    <img src={image4} alt="audio" />
+                </div>
+                <h5>Decor & Props</h5>
+                <div className="para_button_container">
+                    <p>Custom designed sets that are tailor-made to suit your requirement...</p>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
+
+                </div>
+                <div className="know_more_Button">
+                    <button className="button-7">know more</button>
+                </div>
+            </div>
+
         </div>
         <div className="card_container_2">
+            
+
 
         </div>
     </div>
