3f132b68a3209f9436e69fb4970e63a743a0a8e8
Samuel Fernandes <96002830+Codingholic@users.noreply.github.com>
2023-07-13 18:25:02 +0530

    chat bot done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 
+import { useState } from 'react';
 import './App.css';
 import Home from './components/Home/Home';
+import bot from "./components/Navbar/logo/bot.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Bot } from './components/Home/bot/Bot';
+import { Bot } from './components/Home/bot/Bot';
 // import Bot from './components/Home/bot';
 
 function App() {
+  const [display, setDisplay] = useState("bot hide")
+  const [boticon, setboticon] = useState("boticonsize")
+  const handleclosebot = () => {
+    console.log("clicked")
+    setDisplay("bot hide")
+    setboticon("boticonsize")
+  }
+
+  const handlebotdisplay = () => {
+    console.log("clicked")
+    setDisplay("bot")
+    setboticon("boticonsize hide")
+  }
   return (
     <div className="App">
-                <div className="bot">
-            {/* <Bot /> */}
-          </div>
+      <div onClick={handlebotdisplay} className={boticon}>
+        <img onClick={handlebotdisplay} className={boticon} src={bot} alt='bot' />
+      </div>
+      <div className={display}>
+        <button onClick={handleclosebot} className='button-7 moveclosebutton'>close</button>
+        <Bot />
+      </div>
       <Home />
     </div>
   );
